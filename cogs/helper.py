ac98efff58c1b838f3cbc1775fc8040984f8c586
Pitr1010 <peto.jagercik@centrum.sk>
2025-11-10 09:16:20 +0100

    init cmds for Autodel warns

---
+++ cogs/helper.py
@@ -0,0 +1,28 @@
+import discord
+import json
+import aiofiles
+import os
+from discord import app_commands
+from discord.ext import commands
+from datetime import timedelta
+import asyncio
+
+WARN_FILE = 'data/warns.json'
+
+class Autodel_warns(commands.Cog):
+    """On interaction check and delete warns over a month old"""
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.warnings = self.load_json_sync(WARN_FILE)
+
+    def load_json_sync(self, path: str):
+        if os.path.exists(path):
+            with open(path, 'r', encoding='utf-8') as file:
+                try:
+                    return json.load(file)
+                except json.JSONDecodeError:
+                    return {}
+        return {}
+    async def save_json(self, path: str, data: dict):
+        async with aiofiles.open(path, 'w', encoding='utf-8') as file:
+            await file.write(json.dumps(data, indent=4))
